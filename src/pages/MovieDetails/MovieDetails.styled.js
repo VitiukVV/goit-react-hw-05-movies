252977a56fd333b456c23f3061caadd26536ddbe
Volodymyr Vitiuk <v.v.endeavour@gmail.com>
2023-10-01 21:40:40 +0300

    Create MovieDetails.styled.js

---
+++ src/pages/MovieDetails/MovieDetails.styled.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  display: flex;
+  gap: 20px;
+`;
+
+export const GanreList = styled.ul`
+  display: flex;
+  text-decoration: none;
+  list-style: none;
+  padding: 0;
+  gap: 15px;
+`;
