733fc4532887c38b2541d0a2d12c7b8a50129746
Volodymyr Vitiuk <v.v.endeavour@gmail.com>
2023-10-01 21:39:47 +0300

    Create Layout.jsx

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,23 @@
+import { Suspense } from 'react';
+import { Outlet } from 'react-router-dom';
+import { Container, Header, Main, StyledLink } from './Layout.styled';
+
+const Layout = () => {
+  return (
+    <Container>
+      <Header>
+        <nav>
+          <StyledLink to="/">Home</StyledLink>
+          <StyledLink to="/movies">Movies</StyledLink>
+        </nav>
+      </Header>
+      <Main>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Outlet />
+        </Suspense>
+      </Main>
+    </Container>
+  );
+};
+
+export default Layout;
